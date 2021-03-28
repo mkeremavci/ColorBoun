a99f07e13f22b7e8f0c8d8a033209e3a0b5a521a
Kerem Avcı <51995982+mkeremavci@users.noreply.github.com>
2021-03-28 16:58:24 +0300

    Add files via upload

---
+++ ColorBoun.py
@@ -0,0 +1,15 @@
+'''
+ColorBoun by Kerem Avci
+
+Nanonetwork Research Group, Bogazici University, Istanbul
+
+Colorimetric Analysis in Color Spaces HVS and RGB
+
+2020
+'''
+
+from Package import App
+
+ColorBoun = App.App()
+
+ColorBoun.start_system()
